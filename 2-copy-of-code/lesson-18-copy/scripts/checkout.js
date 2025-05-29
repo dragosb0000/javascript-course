3cfe94cf9bcea5edff432f17a80786fa5c5715a2
Dragos Balhui <dragosb0000@yahoo.com>
2025-05-29 20:30:49 +0400

    Created a copy of Lesson 18 and Loaded Products from the backed

---
+++ 2-copy-of-code/lesson-18-copy/scripts/checkout.js
@@ -0,0 +1,8 @@
+import { renderOrderSummary } from './checkout/orderSummary.js';
+import { renderPaymentSummary } from './checkout/paymentSummary.js';
+import { loadProducts } from '../data/products.js';
+// import '../data/cart-class.js';
+loadProducts(() => {
+    renderOrderSummary();
+    renderPaymentSummary();
+});
